bf901584de512c102b1fcbdd6dbdf1aa384b05fb
nitin-peddabachi <66887018+nitin-peddabachi@users.noreply.github.com>
2026-07-12 14:31:29 -0500

    scaffold feed server project

---
+++ server/config.py
@@ -0,0 +1,8 @@
+WATCHLIST = ["AAPL", "NVDA", "NFLX"]
+
+SOCCER_LEAGUES = ["eng.1"]  # ESPN league slugs; eng.1 = Premier League
+NFL = True
+
+# Local hours during which the display dims (23:00–07:00)
+DIM_START = 23
+DIM_END = 7
